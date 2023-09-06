0d9119a9f7d0e582d1bd55540012f4a73892ff36
Rex Hsu <aexz5476@gmail.com>
2023-09-06 23:44:04 +0800

    install & initialize sequelize

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const db = require('./models')
+const Restaurant = db.Restaurant
+
 app.get('/', (req, res) => {
   res.send('hello world')
+})
+
+app.get('/restaurants', (req, res) => {
+  return Restaurant.findAll()
+    .then((restaurants) => res.send({ restaurants }))
+    .catch((err) => res.status(422).json(err))
 })
 
 app.listen(port, () => {
